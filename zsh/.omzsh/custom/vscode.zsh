a6b909806fe1bde1a9cc90afb0026a410c4133d4
bgore <bgore@nabancard.com>
2023-11-27 17:08:01 -0600

    chore: :spiral_notepad: simplify vscode profile edit

--- zsh/.omzsh/custom/vscode.zsh
+++ zsh/.omzsh/custom/vscode.zsh
@@ -1,2 +1,1 @@
-alias codeProfile.go="code /Users/bgore/Library/Application\ Support/Code/User/profiles/1035f50c"
-alias codeProfile.ts="code /Users/bgore/Library/Application\ Support/Code/User/profiles/28d80b12"
+alias codeProfiles.ed="code /Users/bgore/Library/Application\ Support/Code/User/profiles"
