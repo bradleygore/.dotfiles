e113fa08ffed30d31571c24c7916f15535b3c1de
Bradley Gore <bradleygore@users.noreply.github.com>
2025-03-07 16:01:10 -0600

    Update aws.zsh with awslocal alias

--- zsh/.omzsh/custom/aws.zsh
+++ zsh/.omzsh/custom/aws.zsh
@@ -4,3 +4,6 @@ alias aws.whoami="aws sts get-caller-identity"
 ## change profile like `aws.profile.set some_profile_name`
 alias aws.profile.set="export AWS_DEFAULT_PROFILE=$1; export AWS_PROFILE=$1"
 alias aws.adabraBradley="export AWS_DEFAULT_PROFILE=adabra_bradley"
+
+## awslocal is for localstack usage; see also https://github.com/localstack/awscli-local?tab=readme-ov-file#alternative
+alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
